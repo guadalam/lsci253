4d335ac91e7c7a652e34298b038978594d35873d
guadalam <74213249+guadalam@users.noreply.github.com>
2024-05-21 11:12:27 -0700

    Delete df_may_21.R

--- df_may_21.R
+++
@@ -1,7 +0,0 @@
-#Convert to raw csv file from git#
-url_git <- "https://raw.githubusercontent.com/guadalam/lsci253/main/data/rs.csv"
-
-#Naming data frame#
-df <- read.csv(url(url_git))
-
-               
